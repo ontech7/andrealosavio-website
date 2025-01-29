dbcf773500b90ed35f962305897b00ad4998ece2
Lorenzo Cicuttin <102952974+9elt@users.noreply.github.com>
2025-01-29 17:36:14 +0100

    colour fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="it">
       <Head />
       <body>
+        <style>{`html { --hr: 38deg; filter: hue-rotate(var(--hr)); } img { filter: hue-rotate(calc(360deg - var(--hr))); }`}</style>
         <Main />
         <NextScript />
       </body>
